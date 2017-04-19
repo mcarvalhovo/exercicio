f760e163b89985f265b7e5fdb9b8434561f3142b
mcarvalho <miguel.a.g.carvalho@gmail.com>
2017-04-19 11:56:30 +0100

    Exercico 1

---
+++ w3resource/ex1.js
@@ -0,0 +1,11 @@
+console.log ('\n1. Write a JavaScript program to display the current day and time in the following format. Go to the editor\n');
+//Sample Output : Today is : Friday. 
+//Current time is : 4 PM : 50 : 22
+
+
+if (hour <=11 && minute < 59){
+	time = 'AM';
+	} else 
+	{
+		time = 'PM';
+	}
